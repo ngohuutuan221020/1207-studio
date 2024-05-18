6aae2903ad979dab7cc449d41e1941949119c6ce
Ngo Huu Tuan <ngohuutuan221020@gmail.com>
2024-05-18 12:27:19 +0700

    updateContact

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -2,7 +2,14 @@ import React, { useEffect } from "react";
 import "./Contact.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TopComponent from "./TopComponent";
-import IMG from "../images/74/3.jpg";
+import IMG from "../images/75/3.jpg";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Image from "react-bootstrap/Image";
+import Form from "react-bootstrap/Form";
+
 const Contact = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -10,124 +17,87 @@ const Contact = () => {
   return (
     <>
       <TopComponent />
-      <div className="contact3 py-5">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              <div className="card mt-4 border-0 mb-4">
-                <div className="row">
-                  <div className="col-lg-4 col-md-4">
-                    <div className="card-body d-flex align-items-center c-detail">
-                      <div className="mr-3 align-self-center">
-                        <img
-                          src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/contact/icon1.png"
-                          alt="address"
-                        />
-                      </div>
-                      <div className="">
-                        <h6 className="font-weight-medium">Address</h6>
-                        <p>HCM City I</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-4 col-md-4">
-                    <div className="card-body d-flex align-items-center c-detail">
-                      <div className="mr-3 align-self-center">
-                        <img
-                          src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/contact/icon2.png"
-                          alt="phone"
-                        />
-                      </div>
-                      <div className="">
-                        <h6 className="font-weight-medium">Phone</h6>
-                        <p>0939 0444 79</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-4 col-md-4">
-                    <div className="card-body d-flex align-items-center c-detail">
-                      <div className="mr-3 align-self-center">
-                        <img
-                          src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/contact/icon3.png"
-                          alt="email"
-                        />
-                      </div>
-                      <div className="">
-                        <h6 className="font-weight-medium">Email</h6>
-                        <p
-                          style={{
-                            fontSize: "90%",
-                          }}
-                        >
-                          nguyencongminh.arch@gmail.com
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+      <Container className="py-3">
+        <Row>
+          <Col lg={6} className="m-auto">
+            <Image src={IMG} thumbnail />
+          </Col>
+          <Col lg={6}>
+            <div className="contact-box ml-3">
+              <h1 className="font-weight-light mt-2">Quick Contact</h1>
+              <Form>
+                <Form.Group className="mb-3">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control type="text" placeholder="Name" />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="name@example.com" />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Phone</Form.Label>
+                  <Form.Control type="text" placeholder="Phone" />
+                </Form.Group>
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlTextarea1"
+                >
+                  <Form.Label>Message</Form.Label>
+                  <Form.Control as="textarea" rows={3} placeholder="Message" />
+                </Form.Group>
+              </Form>
             </div>
-            <div className="col-lg-6">
-              <div className="card-shadow">
-                <img src={IMG} className="img-fluid" alt="contact" />
-              </div>
-            </div>
-            <div className="col-lg-6">
-              <div className="contact-box ml-3">
-                <h1 className="font-weight-light mt-2">Quick Contact</h1>
-                <form className="mt-4">
-                  <div className="row">
-                    <div className="col-lg-12">
-                      <div className="form-group mt-2">
-                        <input
-                          className="form-control"
-                          type="text"
-                          placeholder="name"
-                        />
-                      </div>
+          </Col>
+          <Col lg={12}>
+            <Card className="mt-4 border-0 mb-4">
+              <Row>
+                <Col lg={4} md={3}>
+                  <div className="card-body d-flex align-items-center c-detail">
+                    <div className="mx-3 align-self-center">
+                      <img
+                        src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/contact/icon1.png"
+                        alt="address"
+                      />
                     </div>
-                    <div className="col-lg-12">
-                      <div className="form-group mt-2">
-                        <input
-                          className="form-control"
-                          type="email"
-                          placeholder="email address"
-                        />
-                      </div>
-                    </div>
-                    <div className="col-lg-12">
-                      <div className="form-group mt-2">
-                        <input
-                          className="form-control"
-                          type="text"
-                          placeholder="phone"
-                        />
-                      </div>
-                    </div>
-                    <div className="col-lg-12">
-                      <div className="form-group mt-2">
-                        <textarea
-                          className="form-control"
-                          rows="3"
-                          placeholder="message"
-                        ></textarea>
-                      </div>
-                    </div>
-                    <div className="col-lg-12">
-                      <button
-                        type="submit"
-                        className="btn btn-danger-gradiant mt-3 text-white border-0 px-3 py-2"
-                      >
-                        <span> SUBMIT</span>
-                      </button>
+                    <div className="">
+                      <h6 className="font-weight-medium">Address</h6>
+                      <p>HCM City I</p>
                     </div>
                   </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+                </Col>
+                <Col lg={4} md={3}>
+                  <div className="card-body d-flex align-items-center c-detail">
+                    <div className="mx-3 align-self-center">
+                      <img
+                        src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/contact/icon2.png"
+                        alt="phone"
+                      />
+                    </div>
+                    <div className="">
+                      <h6 className="font-weight-medium">Phone</h6>
+                      <p>0939 0444 79</p>
+                    </div>
+                  </div>
+                </Col>
+                <Col lg={4} md={6}>
+                  <div className="card-body d-flex align-items-center c-detail">
+                    <div className="mx-3 align-self-center">
+                      <img
+                        src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/contact/icon3.png"
+                        alt="email"
+                      />
+                    </div>
+                    <div className="">
+                      <h6 className="font-weight-medium">Email</h6>
+                      <p>nguyencongminh.arch @gmail.com</p>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
